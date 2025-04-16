7403e51b0d31736a2b22329b8227a9bdb710f84e
Fermin_Huerta <92596712+RedHair11@users.noreply.github.com>
2025-04-16 11:51:29 -0500

    Update AdminContext.jsx

--- frontend-admin/src/context/AdminContext.jsx
+++ frontend-admin/src/context/AdminContext.jsx
@@ -9,8 +9,8 @@ export const AdminContext = createContext()
 const AdminContextProvider = (props) => {
 
     // Get the backend URL from environment variables
-    const backendUrl = "https://office-hours-backend.onrender.com"
-    // const backendUrl = import.meta.env.VITE_BACKEND_URL
+    //const backendUrl = "https://office-hours-backend.onrender.com"
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     // State to store admin token, fetched from local storage if available
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
