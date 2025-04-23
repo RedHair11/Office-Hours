483f67f2953bb2805b9a37f45ed0d746d9118c2f
Ed-MG128 <emg12894@gmail.com>
2025-04-23 13:34:38 -0500

    Added comments

--- backend/routes/professorRoute.js
+++ backend/routes/professorRoute.js
@@ -19,7 +19,7 @@ professorRouter.post("/complete-appointment", authProfessor, appointmentComplete
 professorRouter.get("/dashboard", authProfessor, professorDashboard)
 professorRouter.get("/profile", authProfessor, professorProfile)
 
-// ✅ UPDATED: Allow image upload for professor profile update
+// UPDATED: Allow image upload for professor profile update
 professorRouter.post("/update-profile", authProfessor, upload.single('image'), updateProfessorProfile)
 
 export default professorRouter
